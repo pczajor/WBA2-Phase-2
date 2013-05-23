06bcacee7016b86d123e9ea5b3099e4524dac6da
pczajor <patman_cz@web.de>
2013-05-23 13:56:15 +0200

    eventsressource

--- WBA2-Phase2/src/resources/EventResource.java
+++ WBA2-Phase2/src/resources/EventResource.java
@@ -1,7 +1,30 @@
 package resources;
-import javax.ws.rs.GET;
+import java.util.List;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import jaxb.*;
 
 
+@Path("resources")
 public class EventResource {
+	
+	private static EType event = new EType();
 
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public EType getEventInfos(int ID) {
+		return eventsresource.events.getEvent()
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public int putevent(){
+		return 1;
+	}
+	
+
+	
+	
 }
--- WBA2-Phase2/src/resources/eventsresource.java
+++ WBA2-Phase2/src/resources/eventsresource.java
@@ -1,5 +1,75 @@
 package resources;
 
+import jaxb.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import com.sun.jersey.api.NotFoundException;
+
+
+@Path("recources")
 public class eventsresource {
+
+	public static EventsType events = new EventsType();
+
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public List<EType> getEvents() {
+		return events.getEvent();
+	}
+
+	/*
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response postevent(EType event){
+		URI location = addevent(event);
+
+		return Response.created(location).build();
+	}*/
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response postevent(@FormParam("Spielzeitaum") DType spielzeitraum,
+			@FormParam("Sportart") SType sportart,
+			@FormParam("Oertlichkeit") OType oertlichkeit,
+			@FormParam("Spielerliste") SlType spielerliste,
+			@FormParam("Blacklist") BType blacklist,
+			@FormParam("Admin") SpielerType admin) {
+		if (spielzeitraum == null || sportart == null || oertlichkeit == null
+				|| admin == null) {
+			throw new WebApplicationException(
+					Response.status(400)
+							.entity("Event konnte nicht angelegt werden, da wichtige Informationen fehlen.")
+							.build());
+		}
+		EType veranstaltung = new EType();
+
+		veranstaltung.setSpielzeitraum(spielzeitraum);
+		veranstaltung.setSportart(sportart);
+		veranstaltung.setOertlichkeit(oertlichkeit);
+		veranstaltung.setSpielerliste(spielerliste);
+		veranstaltung.setBacklist(blacklist);
+		veranstaltung.setAdmin(admin);
+
+		return Response.created(location).build();
+
+	}
 
 }
