74a651a3992b9c50d781bc2f437ba3d571bf5b41
pczajor <patman_cz@web.de>
2013-05-24 12:26:31 +0200

    spielerresourece

--- WBA2-Phase2/src/resources/spielerresource.java
+++ WBA2-Phase2/src/resources/spielerresource.java
@@ -1,5 +1,19 @@
 package resources;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+
+import jaxb.EType;
+import jaxb.EventsType;
+
 public class spielerresource {
+	
+	public spielerresource() throws Exception {
+		
+	}
 
 }
